86f527d001c2094096383453397ec58d88b6cff9
Marcelo Gomes Ferreira <marcelooferreira@outlook.com.br>
2019-07-07 15:09:20 -0300

    Começando o footer

---
+++ dist/js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $('.Slider_Movies').slick({
+        setting- name: setting - value
+    });
+});
+
+$('.Slider_Movies_Item').slick({
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 6150,
+    // variableWidth: true,
+    arrows: false,
+    infinite: true,
+    fade: true,
+    cssEase: 'linear'
+});
